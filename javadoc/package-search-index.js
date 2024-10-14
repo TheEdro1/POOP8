cf6afc30c5d3a714a2e247e8dd56374c87eb5701
TheEdro1 <162408477+TheEdro1@users.noreply.github.com>
2024-10-14 14:54:25 -0600

    Adicion de javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poop8"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop8","l":"Cuadrilatero"},{"p":"poop8","l":"Flauta"},{"p":"poop8","l":"instrumentoDeViento"},{"p":"poop8","l":"instrumentoMusical"},{"p":"poop8","l":"Meses"},{"p":"poop8","l":"Poligono"},{"p":"poop8","l":"POOP8"},{"p":"poop8","l":"Triangulo"}];updateSearchResults();
